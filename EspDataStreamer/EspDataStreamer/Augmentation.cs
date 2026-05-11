31b9892376093049fd3b38e42ef98245d32ab11c
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2026-05-11 19:48:06 +0200

    adjusted effect parameter ranges and disabled broken effects

--- EspDataStreamer/EspDataStreamer/Augmentation.cs
+++ EspDataStreamer/EspDataStreamer/Augmentation.cs
@@ -7,14 +7,14 @@ namespace EspDataStreamer;
 public static class Augmentation
 {
     private static readonly (Action<EsperAudio, Vector<float>>, float, float)[] Fx = [
-        (Effects.Breathiness, -1, 0.8f),
-        (Effects.Brightness, -1, 1),
-        (Effects.Dynamics, -1, 1),
-        (Effects.FormantShift, -1, 1),
-        (Effects.Growl, 0, 1),
-        (Effects.Mouth, -1, 1),
-        (Effects.Roughness, -1, 1),
-        (Effects.Steadiness, -1, 1)
+        (Effects.Breathiness, -0.5f, 0.8f),
+        //(Effects.Brightness, -0.5f, 0.5f),
+        (Effects.Dynamics, -0.5f, 0.5f),
+        (Effects.FormantShift, -0.25f, 0.25f),
+        //(Effects.Growl, 0, 0.5f),
+        //(Effects.Mouth, -0.5f, 0.5f),
+        (Effects.Roughness, -0.5f, 0.5f),
+        (Effects.Steadiness, -0.5f, 0.5f)
     ];
 
     private static void ApplyRandomPitchShift(EsperAudio audio)
